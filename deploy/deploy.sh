e5cd44fa7b991983c5e940aed4e3665a8ebe06ca
Serge Kukharev <serge.kukharev@gmail.com>
2016-12-07 15:15:53 +0100

    Updated a deployment script to include node modules installed. (#5)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,10 +11,16 @@ fi
 
 mkdir -p ~/package/
 
-cd src; zip -r ~/package/"$package_name".zip *; cd ../;
+mkdir -p ~/release/
+mkdir -p ~/release/src
+cp -r src/* ~/release/src/
+cp -r node_modules ~/release/
+cp index.js ~/release/
+
+cd ~/release/; zip -r ~/package/"$package_name".zip *;
 
 aws lambda update-function-code --function-name getOnePost --zip-file fileb://~/package/"$package_name".zip --publish --region "$AWS_REGION"
-aws lambda create-alias --function-name getOnePost --function-version "\$LATEST" --name "${TRAVIS_TAG//./_}" --region ap-southeast-1
+#aws lambda create-alias --function-name getOnePost --function-version "\$LATEST" --name "${TRAVIS_TAG//./_}" --region ap-southeast-1
 
 aws s3 cp ~/package/"$package_name".zip s3://carmudi-deploy-ap/PACKAGES/Service.Content/ --region=ap-southeast-1
 aws s3 cp ~/package/"$package_name".zip s3://carmudi-deploy-eu-central/PACKAGES/Service.Content/ --region=eu-central-1
