8c6b7f6e1636712796e0ea6a7c4d341d685873b5
Serge Kukharev <serge.kukharev@gmail.com>
2016-12-07 15:01:40 +0100

    Updated a deployment script to include node modules installed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
     'use strict';
     grunt.loadNpmTasks('grunt-aws-lambda');
     grunt.initConfig({
-        lambda_invoke: { default: { options: { file_name: 'src/index.js' } } },
+        lambda_invoke: { default: { options: { file_name: 'index.js' } } },
         lambda_package: { default: {} },
         lambda_deploy: { default: { arn: 'arn:aws:lambda:ap-southeast-1:993784152963:function:getOnePost' } }
     });
